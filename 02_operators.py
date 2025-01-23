137bda0ecfce41651dc4853daecd708e4b88139d
Andres Abella <108240062+Daredevil8508@users.noreply.github.com>
2025-01-22 20:14:11 -0500

    Add files via upload

---
+++ 02_operators.py
@@ -0,0 +1,15 @@
+### Operadores ###
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3)
+print(10 // 3)
+print(3 ** 4)## Potencia
+
+print('Hola ' + 'Python ' + 'Hola que tal ') 
+print("Hola " + str(5))
+
+my_float = 2 * 2.5 
+print("Hola" * int(my_float))
